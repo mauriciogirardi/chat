0084fa0cc9d7f8868e6be7c734380dc79b1e8732
Mauricio Girardi <maurigirarde@yahoo.com.br>
2024-03-06 22:09:17 +0000

    change version clerck

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,6 @@
 import { authMiddleware } from '@clerk/nextjs'
-import { NextResponse } from 'next/server'
 
-export default authMiddleware({
-  afterAuth: (auth) => (auth.isPublicRoute ? NextResponse.next() : undefined),
-})
+export default authMiddleware({})
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
