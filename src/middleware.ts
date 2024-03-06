d7b4e7b77f7a07dce29e0c4e76d59b90f3ce5148
Mauricio Girardi <maurigirarde@yahoo.com.br>
2024-03-06 17:28:18 +0000

    test deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-  publicRoutes: ['/sign-in', '/sign-up'],
+  publicRoutes: ['/'],
 })
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!api|trpc))(_next.*|.+.[w]+$)', '/'],
 }
