1d7f5e3cfd4774507d162108d35decfb2e101161
Mauricio Girardi <maurigirarde@yahoo.com.br>
2024-03-08 10:58:02 +0000

    db

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -4,7 +4,9 @@ import { env } from '@/env'
 
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect(env.MONGO_URL)
+    await mongoose.connect(env.MONGO_URL, {
+      connectTimeoutMS: 10000,
+    })
     console.log('MongoDB connected!!')
   } catch (error) {
     console.error(error)
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-  publicRoutes: ['/'],
+  publicRoutes: ['/', '/sing-in', '/sing-up'],
 })
 
 export const config = {
